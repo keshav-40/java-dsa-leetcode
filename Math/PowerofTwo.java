58101512801ffd920f8b46587c19bce61621b487
keshav-40 <keshavsingh19106@gmail.com>
2026-02-05 01:19:01 +0530

    Update PowerofTwo.java

--- Math/PowerofTwo.java
+++ Math/PowerofTwo.java
@@ -1,1 +1,26 @@
+// LeetCode 231: Power of Two
+// Topic: Math / Bit Manipulation
+// Time Complexity: O(1)
+// Space Complexity: O(1)
 
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<1){
+            return false;
+        }
+        else if(n == 1){
+                return true;
+            }
+            else{
+                while(n % 2 == 0){
+                    n = n/2;
+                }
+                if( n == 1){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+}
