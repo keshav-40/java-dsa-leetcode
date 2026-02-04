a02c184721182b32f156e62abf695bba0bc4fc61
keshav-40 <keshavsingh19106@gmail.com>
2026-02-05 01:21:31 +0530

    Update WaterBottles.java

--- Math/WaterBottles.java
+++ Math/WaterBottles.java
@@ -1,1 +1,18 @@
+// LeetCode 1518: Water Bottles
+// Topic: Math / Simulation
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+
+        while(numBottles>=numExchange){
+           int  newBottles = numBottles/numExchange;
+           int remBottles = numBottles%numExchange;
+            ans = ans + newBottles;
+            numBottles = newBottles + remBottles;
+        }
+        return ans;
+    }
+}
