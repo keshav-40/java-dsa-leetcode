945ce6e8059bc890dd2ccd9ee237d9ae6899946e
keshav-40 <keshavsingh19106@gmail.com>
2026-02-05 01:13:59 +0530

    Update TwoSum.java

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,1 +1,20 @@
+// LeetCode 1: Two Sum
+// Topic: Arrays
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+
+        for(int i=0;i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                }
+            }
+        }
+          return ans ;
+    }
+}
